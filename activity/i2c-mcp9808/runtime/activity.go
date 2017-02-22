05d036ec6a446d57797ffc7a7a787026bdf080c8
stevefister <stevefister@gmail.com>
2017-02-22 14:59:15 -0500

    Add files via upload

--- activity/i2c-mcp9808/runtime/activity.go
+++ activity/i2c-mcp9808/runtime/activity.go
@@ -58,7 +58,7 @@ func (a *I2CActivity) Eval(context activity.Context) (done bool, err error) {
 		log.Fatal(err)
 	}
         tmp := float32(t&0x0FFF)/float32(16)
-	context.SetOutput(result, tmp)
+	context.SetOutput(result, int(tmp))
 	//context.SetOutput(float32(t&0x0FFF)/float32(16), 0)	
 	return true, nil
 	
